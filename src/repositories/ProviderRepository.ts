ba09bd43ee66927a68e73dcb48bbfa6ce5f91bff
Ricardo de Paula <flashdepaula@gmail.com>
2020-10-29 20:18:04 +0000

    Criando model e migration de Users

--- src/repositories/ProviderRepository.ts
+++ src/repositories/ProviderRepository.ts
@@ -12,18 +12,18 @@ interface Request {
 @EntityRepository(Provider)
 export default class ProviderRepository extends Repository<Provider> {
     public async findAndUpdate(id:string,{name, cnpj, phone, email}:Request): Promise<Provider>{
-            this.update(Number(id), {
-                name,
-                cnpj,
-                phone,
-                email
-            });
-            const newProvider = await this.findOne(id);
-            if(!newProvider){
-                throw new AppError('Provider do not exist.');
-            }
+        this.update(Number(id), {
+            name,
+            cnpj,
+            phone,
+            email
+        });
+        const newProvider = await this.findOne(id);
+        if(!newProvider){
+            throw new AppError('Provider do not exist.');
+        }
 
-            return newProvider;
-            
+        return newProvider;
+        
     }
 }
